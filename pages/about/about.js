9ba3513453d2949890768657742c7b36b4919efd
zhangrh <zhangrunhao001@163.com>
2017-04-02 23:17:08 +0800

    关于页面, 结构完成, 功能完成

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,1 +1,16 @@
-Page({})
+//about 业务逻辑
+Page({
+    //数据
+    data: {
+        title: "北京弘钰博物古文物鉴定中心",
+        content: ["北京石渠雅集文物鉴定中心是经国家有关部门核准注册，成立于2004年3月，是全国真正具备文物鉴定的机构之一，具有法人地位的文物鉴定单位，是国家正规的文物鉴定中心，第三方的文物鉴定机构，具有严格的鉴定与审核程序。"],
+        images: ["../../images/product.jpg"]
+    },
+    //分享
+    onShareAppMessage: function() {
+        return {
+            title: "古物鉴定中心",
+            path: "/pages/about/about"
+        }
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,9 @@
 //获取应用实例
 var app = getApp()
 Page({
-
+    goAbout: function() {
+        wx.navigateTo({
+            url: "/pages/about/about"
+        })
+    }
 })
